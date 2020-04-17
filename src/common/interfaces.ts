28242a6755404e7f02d0dd067adc1b18e2d93d7d
Christopher Gauffin <christopher.gauffin@gmail.com>
2020-04-17 14:25:49 +0200

    common folder

---
+++ src/common/interfaces.ts
@@ -0,0 +1,31 @@
+// Class interfaces
+export interface EventHandler {
+  addEvents: () => void;
+}
+
+// Specification of socket payloads
+export interface User {
+  id: string;
+}
+
+// TODO Add proper typing of ID
+export interface Room {
+  id: string;
+  source: string;
+}
+
+export interface Chunk {
+  path: string;
+  data: Buffer;
+}
+
+export interface Patch {
+  room: Room;
+  diffs: Diff.ParsedDiff[];
+}
+
+export interface Message {
+  fromUser: User;
+  targetUser: User;
+  message: string;
+}
