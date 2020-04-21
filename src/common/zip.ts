944cf95f34e8382d836282aa1eb4b258128b2121
jtornert <joakim.tornert@outlook.com>
2020-04-21 17:07:14 +0200

    Update zip functionality to a class

--- src/common/zip.ts
+++ src/common/zip.ts
@@ -2,24 +2,34 @@ import extract from 'extract-zip';
 import fs from 'fs-extra';
 import archiver from 'archiver';
 
-export const zipUnpack = (filename: string): void => {
-  const source = 'test.zip';
-  const target = process.cwd() + '/test-repo';
-  extract(source, { dir: target });
-  console.log('unzipped');
-};
+export class Zip {
+  /**
+   *
+   * @param sourcefile - The name of the zip file to be unpacked.
+   * @param dest - The destination folder where the contents will be unpacked. Requires an absolute path.
+   */
+  public zipUnpack = (sourcefile: string, dest: string): void => {
+    extract(sourcefile, { dir: dest });
+    console.log('unzipped');
+  };
 
-export const zipPack = (filename: string): Promise<void> => {
-  const archive = archiver('zip', { zlib: { level: 9 } });
-  const stream = fs.createWriteStream('test-repo.zip');
+  /**
+   *
+   * @param sourcefile - The name of the file or folder to be zipped.
+   * @param dest - The destination folder to write the zip archive. Requires relative path.
+   */
+  public zipPack = (sourcefile: string, dest: string): Promise<void> => {
+    const archive = archiver('zip', { zlib: { level: 9 } });
+    const stream = fs.createWriteStream(dest);
 
-  return new Promise((resolve, reject) => {
-    archive
-      .directory(filename, false)
-      .on('error', err => reject(err))
-      .pipe(stream);
+    return new Promise((resolve, reject) => {
+      archive
+        .directory(sourcefile, false)
+        .on('error', err => reject(err))
+        .pipe(stream);
 
-    stream.on('close', () => resolve());
-    archive.finalize();
-  });
-};
+      stream.on('close', () => resolve());
+      archive.finalize();
+    });
+  };
+}
