0ddce42643a85b50f3b9f830684ead12fac5c21d
Christopher Gauffin <christopher.gauffin@gmail.com>
2020-04-22 17:04:06 +0200

    getters and setters for Socket

---
+++ src/common/interfaces.ts
@@ -0,0 +1,38 @@
+// Class interfaces
+export interface EventHandler {
+  addEvents: () => void;
+}
+
+// Specification of socket payloads
+export interface User {
+  id: string;
+}
+
+export interface Room {
+  id: string;
+  source: string;
+}
+
+export interface IChunk {
+  source: string;
+  progress: number;
+  done: boolean;
+  data: Buffer;
+}
+
+export interface Patch {
+  room: Room;
+  diffs: Diff.ParsedDiff[];
+}
+
+export interface Message {
+  fromUser: User;
+  targetUser: User;
+  message: string;
+}
+
+export interface IRoom {
+  roomID: string;
+  roomFolderPath: string;
+  sourceFolderPath: string;
+}
