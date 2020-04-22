108c24466394ccd329878cf08e201fe1ce8675b1
taumich <michael.arenander@gmail.com>
2020-04-22 14:01:17 +0200

    Comments on the shadowHandler and SourceFolderHandler

--- src/fs/shadowHandler.ts
+++ src/fs/shadowHandler.ts
@@ -3,37 +3,51 @@ import { FileEventType } from './fileEventType';
 import path from 'path';
 
 /**
- * The FileSystem functions are used for handling file operations such as creating, removing and moving files and folders.
- * @author: Michael Arenander
- * Written: 2020-04-21
+ * The ShadowHandler class is used for handling file operations such as creating, removing and moving files and folders.
+ * The ShadowHandler is used for executing the file/folder changes that were received from the server.
+ * @author Michael Arenander peer: Marcus Alevärn
+ * @date 2020-04-22
  */
 
 export class ShadowHandler {
-  private shadowFolder;
+  // local string where the directory of the client's shadow folder shall be stored.
+  private shadowFolder: string;
 
+  /**
+   * This constructor should be called once per connection and should run until connection closes.
+   * @param sourceFolder path to the root folder that shall be synced and shadowed.
+   */
   constructor(sourceFolder: string) {
-    console.log('Shadow Handler');
+    console.log('Shadow Handler has been constructed with param: ' + sourceFolder);
+
+    // setting the local variable shadowFolder to the directory of the shadow folder
     this.shadowFolder = path.join(sourceFolder, '.shadow');
+
+    // creating the shadow folder at the shadowFolder directory.
     fs.ensureDirSync(this.shadowFolder);
   }
 
   /**
-   *
+   * The update method should be called when a file change has been received from the server.
+   * This file change then results in the coresponding modification of the source folder.
    * @param event
    * @param path example: 'src/index.ts'
    */
   public update(event: FileEventType, relativePath: string, fileContent?: string): void {
+    const operationPath = path.join(this.shadowFolder, relativePath);
+
+    // runs the appropriate file operation that was sent from the server and received to this client.
     switch (event) {
       case FileEventType.FILE_CREATED:
       case FileEventType.FILE_MODIFIED:
-        fs.writeFileSync(path.join(this.shadowFolder, relativePath), fileContent);
+        fs.writeFileSync(operationPath, fileContent);
         break;
       case FileEventType.DIR_CREATED:
-        fs.ensureDirSync(path.join(this.shadowFolder, relativePath));
+        fs.ensureDirSync(operationPath);
         break;
       case FileEventType.FILE_DELETED:
       case FileEventType.DIR_DELETED:
-        fs.removeSync(path.join(this.shadowFolder, relativePath));
+        fs.removeSync(operationPath);
         break;
       default:
         console.error('Error!');
@@ -41,12 +55,10 @@ export class ShadowHandler {
     }
   }
 
+  /**
+   * The getShadowFolder method returns the directory path to the shadow folder.
+   */
   public getShadowFolder(): string {
     return this.shadowFolder;
   }
 }
-
-const shadowTest = new ShadowHandler('test-repo');
-
-shadowTest.update(FileEventType.DIR_CREATED, 'test-folder1');
-shadowTest.update(FileEventType.DIR_DELETED, 'test-folder1');
--- src/fs/sourceFolderHandler.ts
+++ src/fs/sourceFolderHandler.ts
@@ -1,25 +1,45 @@
 import { FileEventType } from './fileEventType';
 import { SourceFolderWatcher } from './sourceFolderWatcher';
 import fs from 'fs';
+import path from 'path';
+
 /**
- * The FileSystem functions are used for handling file operations such as creating, removing and moving files and folders.
- * @author: Michael Arenander
- * Written: 2020-04-21
+ * The SourceFolderHandler class uses the sourceFolderWatcher to receive file and folder updates and then
+ * transmitts them to the
+ * @author Michael Arenander peer: Marcus Alevärn
+ * @date 2020-04-22
  */
 
 export class SourceFolderHandler {
+  // local SourceFolderWatcher-object that will listen to file changes.
   private sourceFolderWatcher;
 
+  /**
+   *This constructor should be called once per connection and should run until connection closes.
+   * @param sourceFolderPath
+   */
   constructor(sourceFolderPath: string) {
-    console.log('Source Folder Handler');
+    console.log('Source Folder Handler has been constructed with param: ' + sourceFolderPath);
+
+    // setting the local variable sourceFolderWatcher to a SourceFolderWatcher-object.
     this.sourceFolderWatcher = new SourceFolderWatcher(sourceFolderPath);
-    this.sourceFolderWatcher.onFileChange((event: FileEventType, path: string) => {
+
+    // starting to listen to any file changes from the sourceFolderWatcher.
+    this.sourceFolderWatcher.onFileChange((event: FileEventType, relativePath: string) => {
       if (event == FileEventType.FILE_CREATED || event == FileEventType.FILE_MODIFIED) {
-        const content = fs.readFileSync(path).toString(); //content
+        const content = fs.readFileSync(relativePath).toString(); //content
+
+        console.log(
+          'Should send file ' + event + '-operation to the server with path=' + relativePath + ' and content=' + content
+        );
+
         // TODO: send event, path and content to server
       } else {
+        console.log('Should send file/folder ' + event + '-operation to the server with path=' + relativePath);
         // TODO: send event and path to server
       }
     });
   }
 }
+
+new SourceFolderHandler('test-repo');
