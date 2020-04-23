511b61bd8c3b54e92c6bcd1b8645759373cb7d78
erikschmutz <erik.rehn98@gmail.com>
2020-04-23 08:27:35 +0200

    added docs on echo

--- src/api/routes/echo.ts
+++ src/api/routes/echo.ts
@@ -2,11 +2,30 @@ import { Route, RouteParams, RouteParamsTypes } from './route';
 import { Request, Response } from 'express';
 import { ParamsDictionary, Query } from 'express-serve-static-core';
 
+/**
+ * Class which echos the response from the user
+ * mainly used for testing.
+ */
 export class EchoRoute extends Route {
-  public description = 'This echos the response';
-  public name = 'echo';
-  public shortName = 'e';
-  public params = {
+  /**
+   * This is the description for the route. This will be used
+   * with the help command.
+   */
+  protected readonly description = 'This echos the response';
+  /**
+   * Every route requires a name. This the route on the server
+   * aswell as the name of the command.
+   */
+  protected readonly name = 'echo';
+  /**
+   * The short name which can be used as well as the long name.
+   */
+  protected readonly shortName = 'e';
+  /**
+   * The parameters which is used by the route. These are given
+   * are query params when using the server.
+   */
+  protected readonly params = {
     repeat: {
       optional: true,
       type: RouteParamsTypes.NUMBER
@@ -16,8 +35,21 @@ export class EchoRoute extends Route {
       type: RouteParamsTypes.BOOLEAN
     }
   };
-  public numberOfArguments = 1;
 
+  /**
+   * The number of argument which is needed with this route.
+   * These are represented as subroutes in the url
+   *
+   * ex : echo/my-phrase
+   */
+  protected readonly numberOfArguments = 1;
+
+  /**
+   * Function which is called when executing the route
+   * @param req the request from the user
+   * @param res a helper object which can send information to
+   * the user.
+   */
   public exec(req: Request<ParamsDictionary, any, any, Query>, res: Response): void {
     const event = this.parseReq<{ repeat: string; reverse: string }>(req);
     const { args, params } = event;
