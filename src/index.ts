f166901b17cadf8f6fb9790b143642cb12ffde87
Christopher Gauffin <christopher.gauffin@gmail.com>
2020-04-27 08:41:14 +0200

    build script

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
-require('module-alias/register');
+const moduleAlias = require('module-alias');
+
+const destPath = process.argv[1].split(/index\...$/)[0];
+
+console.log('>>>>>>>' + destPath);
+moduleAlias.addAliases({
+  '@src': `${destPath}`,
+  '@api': `${destPath}/api`,
+  '@cli': `${destPath}/cli`,
+  '@database': `${destPath}/database`,
+  '@classes': `${destPath}/classes`,
+  '@common': `${destPath}/common`,
+  '@events': `${destPath}/events`
+});
+
 import { CLI } from './cli/cli';
 import Socket from './socket';
 
