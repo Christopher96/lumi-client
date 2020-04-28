979067e895c5de2add73b51948ecdbe0e3cef597
Christopher Gauffin <christopher.gauffin@gmail.com>
2020-04-28 15:09:38 +0200

    sending zips now uses buffers instead and is streamed reliably

---
+++ src/common/copy.ts
@@ -0,0 +1,31 @@
+// import fs from 'fs-extra';
+// import { Chunk, Room } from './interfaces';
+//
+// // Takes a source and destination and copy source recursively
+// export const copySource = (source: string, copy: string): Promise<void> => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(source).then(stat => {
+//       // Check if the source is a directory
+//       if (stat.isDirectory()) {
+//         // Create the copy directory
+//         if (fs.existsSync(copy)) {
+//           fs.removeSync(copy);
+//         }
+//         // Copies the source repo to a copy recursively
+//         fs.copy(source, copy, err => {
+//           if (err) {
+//             reject(`Failed to copy: ${err}`);
+//           }
+//           resolve();
+//         });
+//       } else {
+//         reject('Source is not a directory');
+//       }
+//     });
+//   });
+// };
+//
+// export const writeChunk = (chunk: Chunk): Promise<void> => {
+//   // TODO Create write stream to handle larger files
+//   return fs.outputFile(chunk.path, chunk.data, { flag: 'w' });
+// };
---
+++ src/common/patch.ts
@@ -0,0 +1,50 @@
+import * as Diff from 'diff';
+import readFileGo from 'readfile-go';
+import fs from 'fs-extra';
+import path from 'path';
+
+// Apply a patch from another client
+export const patchApply = (diffs: Diff.ParsedDiff[]): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    // For each specific file patch in the patch
+    diffs.forEach(patch => {
+      // Which file do we want to patch?
+      const filePath = patch.index;
+
+      // Get the old data from the file we want to change
+      const oldData = readFileGo(filePath);
+
+      // Apply the patch to the old data
+      const appliedData = Diff.applyPatch(oldData, patch);
+
+      // Check wheter the patch is valid or not
+      if (appliedData !== 'false') {
+        // Write the new data the the file
+        fs.writeFile(filePath, appliedData, err => {
+          if (err) reject('could not write to file.');
+        });
+      } else {
+        reject('could not apply patch.');
+      }
+    });
+
+    resolve();
+  });
+};
+
+export const patchCreate = (
+  shadowFolderPath: string,
+  sourceFolderPath: string,
+  filePath: string
+): Diff.ParsedDiff[] => {
+  const shadowFile = path.join(shadowFolderPath, filePath);
+  const sourceFile = path.join(sourceFolderPath, filePath);
+
+  const shadowData = readFileGo(shadowFile);
+  const sourceData = readFileGo(sourceFile);
+
+  const patchData = Diff.createTwoFilesPatch(shadowFile, sourceFile, shadowData, sourceData);
+
+  return Diff.parsePatch(patchData);
+  // TODO Optimize this part
+};
