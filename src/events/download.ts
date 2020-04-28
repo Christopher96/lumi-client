97a076fcf87c0377c04dcd8eff29b82dbcddb010
Christopher Gauffin <christopher.gauffin@gmail.com>
2020-04-28 15:09:20 +0200

    sending zips now uses buffers instead and is streamed reliably

--- src/events/download.ts
+++ src/events/download.ts
@@ -5,32 +5,32 @@ import Zip from '@common/zip';
 import Socket from '@src/socket';
 
 export class DownloadEvents implements EventHandler {
+  private tempZip: Buffer;
+
   constructor() {
+    this.tempZip = Buffer.alloc(0);
     this.addEvents();
+  }
+
+  clearZipBuffer() {
+    this.tempZip = Buffer.alloc(0);
   }
 
   addEvents(): void {
     // When we get a file from the client
     Socket.get().on(events.DOWNLOAD_CHUNK, (ichunk: IChunk) => {
       const { room, chunk } = ichunk;
-      const zipPath = `${room.shadowFolderPath}/${chunk.source}`;
 
-      // Write it locally
-      writeChunk(chunk, zipPath)
-        .then(() => {
-          if (chunk.progress == 1) {
-            // We acknowledge and respond to the incoming file
-            Socket.get().emit(events.UPLOAD_DONE, ichunk);
-            const zip = new Zip();
-            zip.unpack(zipPath, room.shadowFolderPath);
-          } else {
-            Socket.get().emit(events.UPLOAD_OK, ichunk);
-          }
-        })
-        .catch(() => {
-          // We could not write the file
-          Socket.get().emit(events.UPLOAD_ERR, ichunk);
-        });
+      this.tempZip = Buffer.concat([this.tempZip, ichunk.chunk.data]);
+
+      if (chunk.progress == 1) {
+        // We acknowledge and respond to the incoming file
+        Socket.get().emit(events.UPLOAD_DONE, ichunk);
+        new Zip().unpack(this.tempZip, `${room.sourceFolderPath}/${room.shadowFolderPath}`);
+        this.clearZipBuffer();
+      } else {
+        Socket.get().emit(events.UPLOAD_OK, ichunk);
+      }
     });
   }
 }
