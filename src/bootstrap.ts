e0a903f31194c6b56b64ae4c47603c2ed316f46f
Marcus Alevärn <marcus.alevarn@gmail.com>
2020-05-05 11:32:51 +0200

    restored the AWS endpoint

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -5,6 +5,7 @@ import path from 'path';
 import CLI from './cli';
 import 'reflect-metadata';
 import dotenv from 'dotenv';
+process.env.SERVER_ENDPOINT = 'http://it-pr-itpro-duw4azjoa0r0-1588304925.eu-west-1.elb.amazonaws.com';
 export default class Bootstrap {
   static init(): void {
     if (process.env.NODE_ENV === 'prod') {
