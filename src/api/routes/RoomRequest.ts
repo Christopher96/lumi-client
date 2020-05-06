d3406dc4039379fe53caf9b10eb12d70843049e6
Christopher Gauffin <christopher.gauffin@gmail.com>
2020-05-06 13:35:49 +0200

    modularized code so we can use API from CLI and React

--- src/api/routes/RoomRequest.ts
+++ src/api/routes/RoomRequest.ts
@@ -1,9 +1,9 @@
 import { API, DefaultServerResponse } from '../API';
 import io from 'socket.io-client';
 import { Events } from './SocketEvents';
-import { Console } from '../../lib/utils/Console';
 import { Config } from '../../lib/utils/Config';
 import { FS } from '../../lib/common/FS';
+import { FileEvent, IFileChange, IPatch, FileEventRequest } from '../../lib/common/types';
 
 export class RoomRequest {
   static create(buffer: Buffer) {
@@ -26,37 +26,22 @@ export class RoomRequest {
     return new API().download('/room/download/' + roomId);
   }
 
-  static joinRoom(roomId: string): Promise<SocketIOClient.Socket> {
+  static joinRoom(roomId: string, sourceFolderPath: string): Promise<SocketIOClient.Socket> {
     // Create the server instance with the server
     const socket = io(process.env.SERVER_ENDPOINT, {
       transports: ['websocket']
     });
 
-    socket.on(Events.room_join_err, error => {
-      throw new Error(error.message);
-    });
-
     return new Promise(resolve => {
       socket.once('connect', () => {
-        // After emitting Events.room_kick we should get this response (if the person got kicked).
-        socket.on(Events.room_kick_res, obj => {
-          Console.error(obj.message);
-          process.exit();
-        });
-
-        // After emitting Events.room_kick we should get this response (if the server failed to kick).
-        socket.on(Events.room_kick_err, obj => {
-          Console.error(obj.message);
-        });
-
-        // After emitting Events.room_leave we should get this response (if everything went well).
-        socket.on(Events.room_leave_res, () => {
-          Console.yellow('You have left the room');
-        });
-
-        // After emitting Events.room_leave we should get this response (if it failed).
-        socket.on(Events.room_leave_err, obj => {
-          Console.error(obj.message);
+        socket.on(Events.room_file_change_res, async (fileEventRequest: FileEventRequest) => {
+          if (fileEventRequest.change.event === FileEvent.FILE_MODIFIED) {
+            const patch = fileEventRequest.change as IPatch;
+            await FS.applyPatches(sourceFolderPath, patch);
+          } else {
+            const fileChange = fileEventRequest.change as IFileChange;
+            await FS.applyFileChange(sourceFolderPath, fileChange);
+          }
         });
 
         // If the server asks for our public config read the config and send it back to the server.
@@ -65,12 +50,16 @@ export class RoomRequest {
             .then(conf => socket.emit(Events.public_config_res, { config: conf.public }))
             .catch(err => socket.emit(Events.public_config_err, 'Failed to get config'));
         });
+
+        FS.listenForLocalFileChanges(sourceFolderPath, (fileChange: IFileChange) => {
+          socket.emit(Events.room_file_change, { change: fileChange, roomId });
+        });
+        FS.listenForLocalPatches(sourceFolderPath, (patch: IPatch) => {
+          socket.emit(Events.room_file_change, { change: patch, roomId });
+        });
+
+        resolve(socket);
       });
-
-      // Tell the server we would like to join.
-      socket.emit(Events.room_join, roomId);
-
-      resolve(socket);
     });
   }
 }
