2267f2167138dad41d85204cd0bbd832e549a009
erikschmutz <erik.rehn98@gmail.com>
2020-05-06 22:41:39 +0200

    removed shadow

--- src/lib/utils/Console.ts
+++ src/lib/utils/Console.ts
@@ -8,21 +8,27 @@ export class Console {
   static success(...successMessage: any[]) {
     console.log(chalk.green(...successMessage));
   }
+
   static error(...error: any[]) {
     console.log(chalk.red(...error));
   }
+
   static blue(...message: any[]) {
     console.log(chalk.blue(...message));
   }
+
   static green(...message: any[]) {
     console.log(chalk.green(...message));
   }
+
   static yellow(...message: any[]) {
     console.log(chalk.yellow(...message));
   }
+
   static warn(...message: any[]) {
     console.log(chalk.yellow(...message));
   }
+
   static log(...message: any[]) {
     console.log(...message);
   }
