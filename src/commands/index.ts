212b4fef178c6575b628cb733779ab964c0e763d
Christopher Gauffin <christopher.gauffin@gmail.com>
2020-05-06 11:19:50 +0200

    index for commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,1 +1,7 @@
+export * from './checkRoom';
+export * from './createRoom';
 export * from './echo';
+export * from './joinRoom';
+export * from './listRoom';
+export * from './listUsersInRoom';
+export * from './ping';
