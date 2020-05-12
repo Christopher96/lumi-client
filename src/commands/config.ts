43760689d195c90e5166b9c1d66e1de4d0454814
Marcus Alevärn <marcus.alevarn@gmail.com>
2020-05-12 13:58:47 +0200

    fixed the add avatar command

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -1,5 +1,26 @@
 import { Console } from '../lib/utils/Console';
 import { Config, IConfig } from '../lib/utils/Config';
+import fse from 'fs-extra';
+import path from 'path';
+
+const validExtensions: string[] = ['.png', '.jpg'];
+
+/**
+ * Check if the picture exists and has a valid extension.
+ * @param picturePath 
+ */
+function isValidAvatar(picturePath: string): boolean {
+  if (!fse.pathExistsSync(picturePath)) {
+    Console.error(`Could not find picture: ${picturePath}`);
+    return false;
+  }
+  if (!validExtensions.find(ext => ext === path.extname(picturePath.toLowerCase()))) {
+    Console.error(`Illegal picture extension: ${path.extname(picturePath)}`);
+    return false;
+  }
+
+  return true;
+}
 
 export const configCommand = async obj => {
   if (obj.set) {
@@ -9,6 +30,17 @@ export const configCommand = async obj => {
     switch (obj.set) {
       case 'username':
         config.public.base.username = value;
+        break;
+      case 'avatar':
+        {
+          // If the user wants to remove the avatar it can be done with 'none'.
+          if (value === 'none') {
+            config.public.extended.avatar = null;
+          } else {
+            const picturePath = path.join(process.cwd(), value);
+            if (isValidAvatar(picturePath)) config.public.extended.avatar = fse.readFileSync(picturePath);
+          }
+        }
         break;
       default:
         Console.error(`You cannot set ${obj.set}`);
